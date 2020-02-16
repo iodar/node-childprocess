18741363887a8067fc2543f00fa7aba4bd9699de
iodar <granzowdario@gmail.com>
2020-02-16 17:58:03 +0100

    test: added tests, libs and support for tests

--- dockerComposeAsync.js
+++ dockerComposeAsync.js
@@ -1,23 +1,28 @@
 const { spawn } = require("child_process");
 
 // todo: 2020-02-15 iodar how to provide params to the promise function?
-const dockerCompose = new Promise((resolve, reject) => {
-  const dockerComposeProc = spawn("docker-compose", ["--version"]);
-  let procByteData = "";
-  dockerComposeProc.stdout.on("data", dataChunk => {
-    procByteData += dataChunk;
+const dockerCompose = commandString =>
+  new Promise((resolve, reject) => {
+    const dockerComposeProc = spawn("docker-compose", [commandString]);
+    let procByteData = "";
+    dockerComposeProc.stdout.on("data", dataChunk => {
+      procByteData += dataChunk;
+    });
+
+    dockerComposeProc.on("close", exitCode => {
+      if (exitCode !== 0) {
+        reject(`command failed with exit code ${exitCode}`);
+      } else {
+        resolve(procByteData);
+      }
+    });
   });
 
-  dockerComposeProc.on("close", exitCode => {
-    if (exitCode !== 0) {
-      reject("command failed");
-    } else {
-      resolve(procByteData);
-    }
+dockerCompose("-v")
+  .then(data => {
+    const dataString = data.toString();
+    console.log(dataString);
+  })
+  .catch(err => {
+    console.log(err);
   });
-});
-
-dockerCompose.then(data => {
-  const dataString = data.toString();
-  console.log(dataString);
-});
---
+++ test/dummy.spec.js
@@ -0,0 +1,8 @@
+const { before } = require("mocha");
+const { expect } = require("chai");
+
+describe("should do sth", () => {
+  it("sth", () => {
+    expect(true).to.be.eq(true);
+  });
+});
