c4b873b52d7f858677e9dae5e4e4ff756bf8c084
iodar <granzowdario@gmail.com>
2020-02-16 18:09:29 +0100

    docs: added todo

--- test/dummy.spec.js
+++ test/dummy.spec.js
@@ -1,6 +1,10 @@
 const { before } = require("mocha");
 const { expect } = require("chai");
 
+before(() => {
+  // todo: 2020-02-16 iodar clean up docker containers and images
+});
+
 describe("should do sth", () => {
   it("sth", () => {
     expect(true).to.be.eq(true);
